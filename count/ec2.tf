3cd59b50455a14e13e4daccd9a4a017c2e4a838f
Sahasra-DevSecops <uicloudops@gmail.com>
2026-06-12 15:12:27 +0530

    added outputs.tf in count

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,12 +1,8 @@
 resource "aws_instance" "example" {
-  count = 10
-  # ami                    = var.ami_id
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.micro"
-
-  #instance_type          = var.instance_type
+  count                  = 10
+  ami                    = "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  #tags                   = var.ec2_tags
   tags = {
     Name        = var.instances_name[count.index]
     Project     = "roboshop"
